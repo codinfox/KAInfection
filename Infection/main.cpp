87266f4234ebdd7921dc4d389b0005ba3087dc05
Zhihao <lzh727@126.com>
2015-10-23 22:20:27 -0400

    Initial Commit

---
+++ Infection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Infection
+//
+//  Created by Zhihao Li on 10/23/15.
+//  Copyright © 2015 TAC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
