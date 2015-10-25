68150572aa2a463d6e1933294a72759cb79327c4
Zhihao <lzh727@126.com>
2015-10-25 16:08:07 -0400

    Test case generator
    C++ codes to generate test cases in the format of the real c++ code.
    Generated codes can be used directly in the program (by pasting). This
    is not the best way to test the program, but is a fast and
    straightforward way.
    
    The binary file is compiled with:
    Apple LLVM version 7.0.0 (clang-700.0.72)
    Target: x86_64-apple-darwin15.0.0

---
+++ testcase_generator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  int sz = 1000;
+  if (argc > 1) sz = atoi(argv[1]);
+  vector<unordered_set<int> > v(sz);
+  srand(time(0));
+  int amount = 0;
+  while (amount < sz) {
+    int upper = min((int)ceil(rand()%((int)sqrt(sz))), (sz-amount));
+    for (int i = 0; i < upper; i++) {
+        int n = rand() % 5;
+        for (int j = 0; j < n; j ++) {
+          int nn = rand() % (upper);
+          v[i+amount].insert(nn+amount);
+          v[nn+amount].insert(i+amount);
+        }
+      }
+    amount += upper;
+  }
+  
+  for (int i = 0; i < sz; i++) {
+    cout << "User(" << i << ",{";
+    size_t ss = v[i].size();
+    for (auto uu : v[i]) {
+      cout << uu;
+      if (--ss != 0) cout << ",";
+    }
+    cout << "})";
+    if (i != sz-1) cout << ",";
+  }
+}
