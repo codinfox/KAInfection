d5e1cf08c0a2a5057568faeda52667aae8f3da51
Zhihao <lzh727@126.com>
2015-10-25 19:54:29 -0400

    Unit test boilerplate for Infection

---
+++ Test/testInfection.cpp
@@ -0,0 +1,13 @@
+//
+//  testKVector.cpp
+//  Infection
+//
+//  Created by Zhihao Li on 10/25/15.
+//  Copyright © 2015 TAC. All rights reserved.
+//
+
+#include "catch.hpp"
+
+/*
+ * Since Infection class is essentially an interface to UserGraph, so in order to save time, I don't do unit test on this class. And I assume that the file reading is always successful.
+ */
